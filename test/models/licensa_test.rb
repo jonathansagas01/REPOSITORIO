e20b3e09f35f9f910b7b6cf15634ee85f3d37ce7
jonathansagas01 <102539571+jonathansagas01@users.noreply.github.com>
2022-05-21 11:55:15 -0300

    Delete test directory

--- test/models/licensa_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class LicensaTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/models/user_arquivo_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class UserArquivoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
